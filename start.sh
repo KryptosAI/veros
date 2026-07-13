3bfc5e55f4389f88931583b968c8e6dd84e1df4b
William Weishuhn <william.weishuhn3@gmail.com>
2026-07-13 11:32:06 -0700

    SECURITY: Actually scrub key from start.sh, add to .gitignore

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export DEEPSEEK_API_KEY="${DEEPSEEK_API_KEY:-sk-d2b3a587ee7c4afb8f6b08e0b8072774}"
+export DEEPSEEK_API_KEY="${DEEPSEEK_API_KEY:-YOUR_DEEPSEEK_KEY_HERE}"
 cd "$(dirname "$0")"
 
 while true; do
